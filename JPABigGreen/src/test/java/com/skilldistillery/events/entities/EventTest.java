a3baeede2ffbefb551152f2c4ad860a2be475543
mikematosky <mikematosky@hotmail.com>
2020-06-05 18:44:23 -0400

    finish initial setup

---
+++ JPABigGreen/src/test/java/com/skilldistillery/events/entities/EventTest.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.events.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EventTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Event event;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf= Persistence.createEntityManagerFactory("BigGreenPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em= emf.createEntityManager();
+		event = em.find(Event.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		event= null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(event);
+		assertEquals("0.5", event.getInches() );
+	}
+
+}
